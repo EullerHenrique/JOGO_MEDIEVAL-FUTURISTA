250dc2fec0102b76c7d1a0395b2eab6363741167
EullerHenrique <eullerhenrique@outlook.com>
2020-10-02 16:03:58 -0300

    Updated

--- src/Model/Chain_Of_Responsability/Product_Arqueiro/Defesa_Do_Arqueiro.java
+++
@@ -1,7 +0,0 @@
-package Model.Chain_Of_Responsability.Product_Arqueiro;
-
-import Model.Chain_Of_Responsability.Handler.Escudo;
-
-public abstract class Defesa_Do_Arqueiro extends Escudo {
-
-}
--- src/Model/Chain_Of_Responsability/Product_Robo/Defesa_Do_Robo.java
+++
@@ -1,7 +0,0 @@
-package Model.Chain_Of_Responsability.Product_Robo;
-
-import Model.Chain_Of_Responsability.Handler.Escudo;
-
-public abstract class Defesa_Do_Robo extends Escudo {
-
-}
